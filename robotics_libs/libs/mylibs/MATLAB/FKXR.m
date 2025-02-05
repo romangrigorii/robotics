708f17603d740876ac25821a41ca4bb1e0d6338b
inque <romanvgrigorii@gmail.com>
2025-02-04 18:40:49 -0800

    Setting up directories

--- robotics_libs/libs/mylibs/MATLAB/FKXR.m
+++
@@ -1,7 +0,0 @@
-function X = FKXR(th,L)
-    X = zeros(length(th),2);
-    for i = 1:length(th)
-        X(i+1,1) = X(i,1) + L(i)*sin(sum(th(1:i)));
-        X(i+1,2) = X(i,2) + L(i)*cos(sum(th(1:i)));
-    end
-end
--- robotics_libs/libs/mylibs/MATLAB/JacobianXR.m
+++
@@ -1,9 +0,0 @@
-function J = JacobianXR(th,L)
-J = zeros(2,length(th));
-for i = 1:length(th)
-    for ii = i:length(th)
-        J(1,i) = L(ii)*cos(sum(th(1:ii))) + J(1,i);
-        J(2,i) = -L(ii)*sin(sum(th(1:ii))) + J(2,i);
-    end
-end
-end
